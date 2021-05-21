67ff453c160cbe374f6f8a628eaa87e268bd7c0c
rodrigo12101999 <rodrigo.sandro_10@hotmail.com>
2021-05-21 11:35:16 -0500

    actualizacion 21-05-2021

--- vistas/js/usuario.js
+++ vistas/js/usuario.js
@@ -15,4 +15,17 @@ function mostrarform(flag) {
     }
 }
 
+function cancelarform() {
+    $("#claves").show();
+    mostrarform(false);
+}
+
+function generar(longitud) {
+    long = parseInt(longitud);
+    var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789";
+    var contraseña = "";
+    for (i = 0; i < long; i++) contraseña += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    $("#codigo_persona").val(contraseña);
+}
+
 init();
--- vistas/modulos/paginas/usuarios.php
+++ vistas/modulos/paginas/usuarios.php
@@ -17,7 +17,7 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-    <div class="container">
+    <div class="content">
       <div class="row">
         <div class="col-md-12">
           <div class="card card-primary">
@@ -60,63 +60,60 @@
             </div>
             <div class="card-body panel-body" id="formularioregistros">
               <form action="" name="formulario" id="formulario" method="POST">
-              <div class="modal-content">
-              <div class="row">
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Tipo usuario(*):</label>
-                  <select name="idtipousuario" id="idtipousuario" class="form-control select-picker" required>
-                  </select>
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Departamento(*):</label>
-                  <select name="iddepartamento" id="iddepartamento" class="form-control select-picker" required>
-                
-                  </select>
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Nombre(*):</label>
-                  <input class="form-control" type="hidden" name="idusuario" id="idusuario">
-                  <input class="form-control" type="text" name="nombre" id="nombre" maxlength="100" placeholder="Nombre" required>
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Apellidos(*):</label>
-                  <input class="form-control" type="text" name="apellidos" id="apellidos" maxlength="100" placeholder="Apellidos" required>
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Email: </label>
-                  <input class="form-control" type="email" name="email" id="email" maxlength="70" placeholder="email">
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Login(*):</label>
-                  <input class="form-control" type="text" name="login" id="login" maxlength="20" placeholder="nombre de usuario" required>
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12" id="claves">
-                  <label for="">Clave de ingreso(*):</label>
-                  <input class="form-control" type="password" name="clave" id="clave" maxlength="64" placeholder="Clave">
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12" id="claves">
-                  <label for="">Clave de asistencia(*):</label>
-                  <button class="btn btn-info" type="button" onclick="generar(6);" >Generar</button>
-                  <input class="form-control" type="text" name="codigo_persona" id="codigo_persona" maxlength="64" placeholder="Clave">
-                </div>
-                <div class="form-group col-lg-6 col-md-6 col-xs-12">
-                  <label for="">Imagen:</label>
-                  <input class="form-control filestyle" data-buttonText="Seleccionar foto" type="file" name="imagen" id="imagen">
-                  <input type="hidden" name="imagenactual" id="imagenactual">
-                  <img src="" alt="" width="150px" id="imagenmuestra">
-                </div>
-                <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                  <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i>  Guardar</button>
-                  <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+                <div class="row">
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Tipo usuario(*):</label>
+                    <select name="idtipousuario" id="idtipousuario" class="form-control select-picker" required>
+                    </select>
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Departamento(*):</label>
+                    <select name="iddepartamento" id="iddepartamento" class="form-control select-picker" required>
+                  
+                    </select>
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Nombre(*):</label>
+                    <input class="form-control" type="hidden" name="idusuario" id="idusuario">
+                    <input class="form-control" type="text" name="nombre" id="nombre" maxlength="100" placeholder="Nombre" required>
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Apellidos(*):</label>
+                    <input class="form-control" type="text" name="apellidos" id="apellidos" maxlength="100" placeholder="Apellidos" required>
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Email: </label>
+                    <input class="form-control" type="email" name="email" id="email" maxlength="70" placeholder="email">
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Login(*):</label>
+                    <input class="form-control" type="text" name="login" id="login" maxlength="20" placeholder="nombre de usuario" required>
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12" id="claves">
+                    <label for="">Clave de ingreso(*):</label>
+                    <input class="form-control" type="password" name="clave" id="clave" maxlength="64" placeholder="Clave">
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12" id="claves">
+                    <label for="">Clave de asistencia(*):</label>
+                    <button class="btn btn-info" type="button" onclick="generar(6);" >Generar</button>
+                    <input class="form-control" type="text" name="codigo_persona" id="codigo_persona" maxlength="64" placeholder="Clave">
+                  </div>
+                  <div class="form-group col-lg-6 col-md-6 col-xs-12">
+                    <label for="">Imagen:</label>
+                    <input class="form-control filestyle" data-buttonText="Seleccionar foto" type="file" name="imagen" id="imagen">
+                    <input type="hidden" name="imagenactual" id="imagenactual">
+                    <img src="" alt="" width="150px" id="imagenmuestra">
+                  </div>
+                  <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i>  Guardar</button>
+                    <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                 </div>
               </form>
             </div>
-          </div>         
-        </div>
+          </div>
+        </div>           
       </div>
     </div>
-
-    
-    </div>
   </div>
+</div>  
   <!-- /.content-wrapper -->
