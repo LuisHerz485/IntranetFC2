1cfdfbda261731c8aa3cb2b94548145d3c578594
LuisHerz485 <luis.hernandez.albia@gmail.com>
2021-08-24 16:13:29 -0500

    Cambio de version

--- vistas/modulos/templates/footer.php
+++ vistas/modulos/templates/footer.php
@@ -2,6 +2,6 @@
     <strong>Copyright &copy; 2020-2021 <a href="https://fccontadores.com/" target="_blank">FC Contadores</a>.</strong>
     Todos los derechos reservados.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Versión</b> 2.0.0
+      <b>Versión</b> 2.3.0
     </div>
 </footer>
